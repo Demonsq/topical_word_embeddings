729d586466af78415b3b9ac198bd069bdeb7c5f2
demonsq <ck769098159@gmail.com>
2018-03-29 17:42:22 +0800

    fix bug caused by python package version

--- TWE-1/gensim/models/word2vec_inner.pyx
+++ TWE-1/gensim/models/word2vec_inner.pyx
@@ -17,7 +17,7 @@ from libc.string cimport memset
 cdef extern from "voidptr.h":
     void* PyCObject_AsVoidPtr(object obj)
 
-from scipy.linalg.blas import fblas
+import scipy.linalg.blas as fblas
 
 REAL = np.float32
 ctypedef np.float32_t REAL_t
